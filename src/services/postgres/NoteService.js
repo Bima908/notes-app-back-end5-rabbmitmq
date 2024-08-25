562b6b38570bf25a83b5400d7667e60f325cf3a2
Andrian ITPI <andrian@itpi.co.id>
2024-08-25 16:34:26 +0800

    collaboration implemented

--- src/services/postgres/NoteService.js
+++ src/services/postgres/NoteService.js
@@ -7,8 +7,9 @@ const { mapToTable } = require("../../utils");
 const AuthorizationError = require("../../exceptions/AuthorizationError");
 
 class NoteService {
-  constructor() {
+  constructor(collaborationService) {
     this._pool = new Pool();
+    this._collaborationService = collaborationService;
   }
 
   async addNote({
@@ -34,16 +35,23 @@ class NoteService {
 
   async getNotes(owner) {
     const query = {
-      text: 'SELECT * FROM notes WHERE owner = $1',
+      text: `SELECT notes.* FROM notes
+      LEFT JOIN collaborations ON collaborations.note_id = notes.id
+      WHERE notes.owner = $1 OR collaborations.user_id = $1
+      GROUP BY notes.id`,
       values: [owner],
     };
+
     const result = await this._pool.query(query);
     return result.rows.map(mapToTable);
   }
 
   async getNoteById(id) {
     const query = {
-      text: 'SELECT * FROM notes WHERE id = $1',
+      text: `SELECT notes.*, users.username
+    FROM notes
+    LEFT JOIN users ON users.id = notes.owner
+    WHERE notes.id = $1`,
       values: [id],
     };
 
@@ -99,6 +107,21 @@ class NoteService {
     const note = result.rows[0];
     if (note.owner !== owner) {
       throw new AuthorizationError('Anda tidak berhak mengakses resource ini');
+    }
+  }
+
+  async verifyNoteAccess(noteId, userId) {
+    try {
+      await this.verifyNoteOwner(noteId, userId);
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        throw error;
+      }
+      try {
+        await this._collaborationService.verifyCollaborator(noteId, userId);
+      } catch {
+        throw error;
+      }
     }
   }
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -5,13 +5,15 @@ const mapToTable = ({
   tags, 
   created_at, 
   updated_at, 
+  username,
 }) => ({
   id,
   title,
   body,
-  tags, 
-  created_at,
-  updated_at,
+  tags,
+  createdAt: created_at,
+  updatedAt: updated_at,
+  username,
 });
 
 module.exports = { mapToTable };
